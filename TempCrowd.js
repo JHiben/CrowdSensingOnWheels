fd21a3c2a816de08044afee0bb84e6b70a0df2bb
Jonathan Hiben <jonathan.hiben@hotmail.com>
2016-01-25 19:10:33 +0100

    api + authentication

--- TempCrowd.js
+++ TempCrowd.js
@@ -1,23 +1,44 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+TemperatureReadings = new Mongo.Collection('temperatureReadings');
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
+    // Global API configuration
+    var Api = new Restivus({
+        useDefaultAuth: true,
+        prettyJson: true
+    });
+
+    // Generates: GET, POST on /api/items and GET, PUT, DELETE on
+    // /api/items/:id for the Items collection
+    Api.addCollection(TemperatureReadings, {
+        routeOptions: {
+            authRequired: true
+        }
+    });
+
+    // Maps to: /api/articles/:id
+    // Api.addRoute('temperatureReadings/:id', { authRequired: true }, {
+    //     get: function () {
+    //         return TemperatureReadings.findOne(this.urlParams.id);
+    //     }
+    //     // delete: {
+    //     //     roleRequired: ['author', 'admin'],
+    //     //     action: function () {
+    //     //         if (Articles.remove(this.urlParams.id)) {
+    //     //             return { status: 'success', data: { message: 'Article removed' } };
+    //     //         }
+    //     //         return {
+    //     //             statusCode: 404,
+    //     //             body: { status: 'fail', message: 'Article not found' }
+    //     //         };
+    //     //     }
+    //     // }
+    // });
+    
+}
+
+if (Meteor.isClient) {
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_AND_EMAIL"
+    });
 }
